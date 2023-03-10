d163eff1948430c65c9642bd21608d4cf3a151f6
Maggiexuiscute <your.maggiexuiscute@gmail.com>
2023-03-10 11:41:39 -0600

    add code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const startBtn = $('#start_btn');
 const login = $('#login');
 const playground = $('#playground');
-const RECORD = [[],[],[],[],[],[],[],[],[],[],[]]
+const RECORD = [[],[],[],[],[],[],[],[],[],[],[],[]];
 const UNIT = [
     [],
     [70, 132, 82, 51, 149, 96],
@@ -43,48 +43,52 @@ let e_value = 0;
 let f_value = 0;
 
 function process(num){
-    
+    $('#final_panel').hide();
     let cash = $('#cash').text();
-    console.log()
     let user = $('#user_id').val();
-    
         RECORD[num].push({user:$('#user_id').val()});
         login.hide();
         playground.show();
-        console.log(RECORD);
 
-
+let flag_a = false;
 $('#buy_a').click(function(){
+    
     let buy_num = $('#a_buy').val();
     let price = $('#a_buy').attr('price');
+    let shares = $('#share_a').text();
+    
     
     if(parseInt(cash) < (parseInt(buy_num) * parseInt(price))){
         alert("Have no enough cash.");
     }else{
-        console.log(RECORD[num]['A_Buy'])
         let flag = false;
+        console.log(RECORD[num]);
+        console.log(num)
         RECORD[num].forEach(function(value){
+            console.log(typeof value.A_Buy == "undefined");
             if(value.A_Buy){
+                console.log("run");
                 flag = true;
-                console.log(value.A_Buy);
                 cash = parseInt(cash) + parseInt(value.A_Buy) * parseInt(price);
+                shares = parseInt(shares) - parseInt(value.A_Buy)
                 value.A_Buy = buy_num;
                 cash = parseInt(cash) - parseInt(buy_num) * parseInt(price);
                 $('#cash').text(cash);
-                $('#share_a').text(buy_num);
+                shares = parseInt(shares) + parseInt($('#a_buy').val())
+                $('#share_a').text(shares);
+                
             }
-
         });
 
         if(!flag){
+            shares = $('#share_a').text();
+            console.log("test");
+            console.log(shares);
             RECORD[num].push({A_Buy: buy_num});
             cash = parseInt(cash) - parseInt(buy_num) * parseInt(price);
             $('#cash').text(cash);
-            shares = $('#share_a').text();
-            $('#share_a').text(buy_num);
-            console.log(RECORD)
-            console.log(RECORD[num].A_Buy)
-            console.log(RECORD[num]['A_Buy'])
+            shares = parseInt(shares) + parseInt($('#a_buy').val())
+            $('#share_a').text(shares);
         }
     }
     
@@ -99,7 +103,6 @@ $('#buy_b').click(function(){
     if(parseInt(cash) < (parseInt(buy_num) * parseInt(price))){
         alert("Have no enough cash.");
     }else{
-        console.log(RECORD[num]['B_Buy'])
         let flag = false;
         RECORD[num].forEach(function(value){
             if(value.B_Buy){
@@ -119,7 +122,6 @@ $('#buy_b').click(function(){
             cash = parseInt(cash) - parseInt(buy_num) * parseInt(price);
             $('#cash').text(cash);
             $('#share_b').text(buy_num);
-            console.log(RECORD)
         }
 
     }
@@ -134,12 +136,10 @@ $('#buy_c').click(function(){
     if(parseInt(cash) < (parseInt(buy_num) * parseInt(price))){
         alert("Have no enough cash.");
     }else{
-        console.log(RECORD[num]['C_Buy'])
         let flag = false;
         RECORD[num].forEach(function(value){
             if(value.C_Buy){
                 flag = true;
-                console.log(value.C_Buy);
                 cash = parseInt(cash) + parseInt(value.C_Buy) * parseInt(price);
                 value.C_Buy = buy_num;
                 cash = parseInt(cash) - parseInt(buy_num) * parseInt(price);
@@ -154,9 +154,7 @@ $('#buy_c').click(function(){
             RECORD[num].push({C_Buy: buy_num});
             cash = parseInt(cash) - parseInt(buy_num) * parseInt(price);
             $('#cash').text(cash);
-    
             $('#share_c').text(buy_num);
-            console.log(RECORD)
         }
 
     }
@@ -172,7 +170,6 @@ $('#buy_d').click(function(){
     if(parseInt(cash) < (parseInt(buy_num) * parseInt(price))){
         alert("Have no enough cash.");
     }else{
-        console.log(RECORD[num]['D_Buy'])
         let flag = false;
         RECORD[num].forEach(function(value){
             if(value.D_Buy){
@@ -183,7 +180,6 @@ $('#buy_d').click(function(){
                 cash = parseInt(cash) - parseInt(buy_num) * parseInt(price);
                 $('#cash').text(cash);
                 $('#share_d').text(buy_num);
-            console.log(RECORD)
             }
 
         });
@@ -193,7 +189,6 @@ $('#buy_d').click(function(){
             cash = parseInt(cash) - parseInt(buy_num) * parseInt(price);
             $('#cash').text(cash);
             $('#share_d').text(buy_num);
-            console.log(RECORD)
         }
 
     }
@@ -209,12 +204,10 @@ $('#buy_e').click(function(){
     if(parseInt(cash) < (parseInt(buy_num) * parseInt(price))){
         alert("Have no enough cash.");
     }else{
-        console.log(RECORD[num]['E_Buy'])
         let flag = false;
         RECORD[num].forEach(function(value){
             if(value.E_Buy){
                 flag = true;
-                console.log(value.E_Buy);
                 cash = parseInt(cash) + parseInt(value.E_Buy) * parseInt(price);
                 value.E_Buy = buy_num;
                 cash = parseInt(cash) - parseInt(buy_num) * parseInt(price);
@@ -244,12 +237,10 @@ $('#buy_f').click(function(){
     if(parseInt(cash) < (parseInt(buy_num) * parseInt(price))){
         alert("Have no enough cash.");
     }else{
-        console.log(RECORD[num]['F_Buy'])
         let flag = false;
         RECORD[num].forEach(function(value){
             if(value.F_Buy){
                 flag = true;
-                console.log(value.F_Buy);
                 cash = parseInt(cash) + parseInt(value.F_Buy) * parseInt(price);
                 value.F_Buy = buy_num;
                 cash = parseInt(cash) - parseInt(buy_num) * parseInt(price);
@@ -304,10 +295,7 @@ $('#sell_a').click(function(){
              $('#share_a').text(sell_num);
              
              shares = parseInt(shares) - parseInt(sell_num);
-             console.log(shares);
-             console.log(sell_num);
             $('#share_a').text(shares);
-            console.log(RECORD)
         }
 
 });
@@ -345,10 +333,7 @@ $('#sell_b').click(function(){
              $('#share_b').text(sell_num);
              
              shares = parseInt(shares) - parseInt(sell_num);
-             console.log(shares);
-             console.log(sell_num);
             $('#share_b').text(shares);
-            console.log(RECORD)
         }
 
 });
@@ -386,10 +371,7 @@ $('#sell_c').click(function(){
              $('#share_c').text(sell_num);
              
              shares = parseInt(shares) - parseInt(sell_num);
-             console.log(shares);
-             console.log(sell_num);
             $('#share_c').text(shares);
-            console.log(RECORD)
         }
 
 });
@@ -427,10 +409,7 @@ $('#sell_d').click(function(){
              $('#share_d').text(sell_num);
              
              shares = parseInt(shares) - parseInt(sell_num);
-             console.log(shares);
-             console.log(sell_num);
             $('#share_d').text(shares);
-            console.log(RECORD)
         }
 
 });
@@ -468,10 +447,7 @@ $('#sell_e').click(function(){
              $('#share_e').text(sell_num);
              
              shares = parseInt(shares) - parseInt(sell_num);
-             console.log(shares);
-             console.log(sell_num);
             $('#share_e').text(shares);
-            console.log(RECORD)
         }
 
 });
@@ -509,10 +485,8 @@ $('#sell_f').click(function(){
              $('#share_f').text(sell_num);
              
              shares = parseInt(shares) - parseInt(sell_num);
-             console.log(shares);
-             console.log(sell_num);
             $('#share_f').text(shares);
-            console.log(RECORD)
+
         }
 
 });
@@ -520,40 +494,110 @@ $('#sell_f').click(function(){
 
  $('#next').click(function(){
     let num = parseInt($('#round_num').text());
-    if(num < 11){
-    a_value = parseInt( $('#share_a').text()) * parseInt($('#a_buy').attr('price'));
-    b_value = parseInt( $('#share_b').text()) * parseInt($('#b_buy').attr('price'));
-    f_value = parseInt( $('#share_f').text()) * parseInt($('#f_buy').attr('price'));
-    e_value = parseInt( $('#share_e').text()) * parseInt($('#e_buy').attr('price'));
-    d_value = parseInt( $('#share_d').text()) * parseInt($('#d_buy').attr('price'));
-    c_value = parseInt( $('#share_c').text()) * parseInt($('#c_buy').attr('price'));
-
     let total = a_value + b_value + c_value + d_value + e_value + f_value + parseInt($('#cash').text());
     RECORD[num].push({total_value:total});
-    console.log(RECORD);
 
-    let inputs = document.getElementsByTagName("input");
-    for(let e of inputs){
-        e.value = "0";
-    }
     num ++;
-    $('#round_num').text(num)
-    $('#a_buy').attr('price', UNIT[num][0]);
-    $('#a_unit').text(UNIT[num][0]);
-    $('#b_buy').attr('price', UNIT[num][1]);
-    $('#b_unit').text(UNIT[num][1]);
-    $('#c_buy').attr('price', UNIT[num][2]);
-    $('#c_unit').text(UNIT[num][2]);
-    $('#d_buy').attr('price', UNIT[num][3]);
-    $('#d_unit').text(UNIT[num][3]);
-    $('#e_buy').attr('price', UNIT[num][4]);
-    $('#e_unit').text(UNIT[num][4]);
-    $('#f_buy').attr('price', UNIT[num][5]);
-    $('#f_unit').text(UNIT[num][5]);
+    if(num < 11){
+        a_value = parseInt( $('#share_a').text()) * parseInt($('#a_buy').attr('price'));
+        b_value = parseInt( $('#share_b').text()) * parseInt($('#b_buy').attr('price'));
+        f_value = parseInt( $('#share_f').text()) * parseInt($('#f_buy').attr('price'));
+        e_value = parseInt( $('#share_e').text()) * parseInt($('#e_buy').attr('price'));
+        d_value = parseInt( $('#share_d').text()) * parseInt($('#d_buy').attr('price'));
+        c_value = parseInt( $('#share_c').text()) * parseInt($('#c_buy').attr('price'));
 
-    
-   process(num);
+        let inputs = document.getElementsByClassName("input_panel");
+        for(let e of inputs){
+            e.value = "0";
+        }
+        
+        $('#round_num').text(num)
+        $('#a_buy').attr('price', UNIT[num][0]);
+        $('#a_unit').text(UNIT[num][0]);
+        $('#b_buy').attr('price', UNIT[num][1]);
+        $('#b_unit').text(UNIT[num][1]);
+        $('#c_buy').attr('price', UNIT[num][2]);
+        $('#c_unit').text(UNIT[num][2]);
+        $('#d_buy').attr('price', UNIT[num][3]);
+        $('#d_unit').text(UNIT[num][3]);
+        $('#e_buy').attr('price', UNIT[num][4]);
+        $('#e_unit').text(UNIT[num][4]);
+        $('#f_buy').attr('price', UNIT[num][5]);
+        $('#f_unit').text(UNIT[num][5]);
+
+        $('#chart').attr('src', 'img/Round ' + num + '.png');
+        process(num);
+    }else if(num == 11){
+        $('#next').hide();
+        $('#final_panel').show();
+        $('#input_panel').hide();
+        $('#final_result').hide();
+        $('#header').hide();
+        $('#chart').attr('src', 'img/Round ' + num + '.png');
+        let finalValue = parseInt($('#cash').text());
+        let finalShare = [
+            $('#share_a').text(),
+            $('#share_b').text(),
+            $('#share_c').text(),
+            $('#share_d').text(),
+            $('#share_e').text(),
+            $('#share_f').text(),
+         ]
+        UNIT[num].forEach(function(e,i){
+            finalValue += e * parseInt(finalShare[i]);
+        });
+        $('#final_value').text(finalValue);
+        RECORD[num].push({user:$('#user_id').val()},{fianl_value:finalValue});
+        console.log(RECORD);
+
+        let flattenedData = RECORD.reduce((acc, val) => acc.concat(val), []); // flatten the nested arrays into a single array
+        
+        // let csv = Papa.unparse(
+        //     {
+        //         fields: ["user", "A_Buy", "A_Sell", "B_Buy", "B_Sell", "C_Buy", "C_Sell", "D_Buy", "D_Sell", "E_Buy", "E_Sell", "F_Buy", "F_Sell"],
+        //         data: RECORD
+        //     }
+        //     );
+        const data = RECORD.flatMap(row => {
+                const flatRow = {};
+                row.forEach(obj => Object.assign(flatRow, obj));
+                return flatRow;
+              });
+              const csv = Papa.unparse(data);
+        console.log(csv);
+        //let csv1 = Papa.unparse(RECORD[1]);
+        let blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+        let url = URL.createObjectURL(blob);
+
+        let link = document.getElementById('log');
+        link.setAttribute("href", url);
+        link.setAttribute("download", "log.csv");
+        
     }
  })       
+        // RECORD=[
+        //     [{user: 'we'},{A_Buy: '4'},{B_Buy: '3'},{C_Buy: '3'},{D_Buy: '3'},{total_value: 8937}
         
+        //     ],
+        //     [
+        //         {user: 'we'},
+        //         {C_Buy: '3'},
+        //         {D_Buy: '3'}, 
+        //         {E_Buy: '3'},
+        //         {total_value: 9072},
+        //         {B_Buy: '3'},
+        //         {A_Buy: '4'}   
+        //     ],
+        //     [{user: 'we'},
+        //     {B_Buy: '3'},
+        //     {C_Buy: '3'},
+        //     {D_Buy: '3'},
+        //     {total_value: 8669},
+        //     {A_Buy: '4'}
+        //     ],[
+        //         {user: 'we'},
+        //         {B_Buy: '3'},
+        //         {total_value: 8063}
+        //     ]
+        // ]
         
